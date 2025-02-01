9991087a324bd4fedda8ffda54ba3bbe347e8c38
Pavel <pashu13@list.ru>
2025-02-02 00:16:31 +0300

    IModuleValidator.sol

--- contracts/interfaces/IModuleValidator.sol
+++ contracts/interfaces/IModuleValidator.sol
@@ -5,7 +5,7 @@ import {OperationType} from './IValidator.sol';
 
 /**
  * @title Modular validator interface for native AA
- * @dev Add signature to module or validate existing signatures for acccount
+ * @dev Add signature to module or validate existing signatures for account
  */
 interface IModuleValidator {
   function handleValidation(OperationType operationType, bytes32 signedHash, bytes memory signature) external view returns (bool);
