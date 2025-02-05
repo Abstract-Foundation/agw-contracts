2c62c8e7f7e55d43a108714d8be1024722f7bdcf
Coffee☕️ <coffee@coffeebot.dev>
2025-02-04 22:01:33 -0500

    subgraph

--- goldsky/session-keys/src/session-key-validator.ts
+++ goldsky/session-keys/src/session-key-validator.ts
@@ -5,10 +5,15 @@ import {
   SessionRevoked as SessionRevokedEvent
 } from "../generated/SessionKeyValidator/SessionKeyValidator"
 import {
+  CallSpec,
+  Constraint,
   Disabled,
   Inited,
   SessionCreated,
-  SessionRevoked
+  SessionRevoked,
+  SessionSpec,
+  TransferSpec,
+  UsageLimit
 } from "../generated/schema"
 import { Bytes } from "@graphprotocol/graph-ts"
 
@@ -39,24 +44,70 @@ export function handleInited(event: InitedEvent): void {
 }
 
 export function handleSessionCreated(event: SessionCreatedEvent): void {
+
+  let sessionSpec = new SessionSpec(event.params.sessionHash)
+  sessionSpec.signer = event.params.sessionSpec.signer
+  sessionSpec.expiresAt = event.params.sessionSpec.expiresAt
+
+  let feeLimit = new UsageLimit(event.params.sessionHash.concatI32(event.logIndex.toI32()));
+  feeLimit.limitType = event.params.sessionSpec.feeLimit.limitType
+  feeLimit.limit = event.params.sessionSpec.feeLimit.limit
+  feeLimit.period = event.params.sessionSpec.feeLimit.period
+  feeLimit.save()
+
+  sessionSpec.feeLimit = feeLimit.id
+  
+  let callPolicies: Bytes[] = []
+  let transferPolicies: Bytes[] = []
+
+  for (let i = 0; i < event.params.sessionSpec.callPolicies.length; i++) {
+    let constraints: Bytes[] = []
+    let callPolicy = new CallSpec(sessionSpec.id.concatI32(i));
+    callPolicy.target = event.params.sessionSpec.callPolicies[i].target
+    callPolicy.selector = event.params.sessionSpec.callPolicies[i].selector
+    callPolicy.maxValuePerUse = event.params.sessionSpec.callPolicies[i].maxValuePerUse
+    for (let j = 0; j < event.params.sessionSpec.callPolicies[i].constraints.length; j++) {
+      let constraint = new Constraint(callPolicy.id.concatI32(j));
+      constraint.condition = event.params.sessionSpec.callPolicies[i].constraints[j].condition
+      constraint.index = event.params.sessionSpec.callPolicies[i].constraints[j].index
+      constraint.refValue = event.params.sessionSpec.callPolicies[i].constraints[j].refValue
+      constraint.save()
+      constraints.push(constraint.id)
+    }
+    callPolicy.constraints = constraints
+    let valueLimit = new UsageLimit(callPolicy.id);  
+    valueLimit.limitType = event.params.sessionSpec.callPolicies[i].valueLimit.limitType
+    valueLimit.limit = event.params.sessionSpec.callPolicies[i].valueLimit.limit
+    valueLimit.period = event.params.sessionSpec.callPolicies[i].valueLimit.period
+    valueLimit.save()
+    callPolicy.valueLimit = valueLimit.id
+    callPolicy.save()
+    callPolicies.push(callPolicy.id)
+  }
+  sessionSpec.callPolicies = callPolicies
+  
+  for (let i = 0; i < event.params.sessionSpec.transferPolicies.length; i++) {
+    let transferPolicy = new TransferSpec(sessionSpec.id.concatI32(i));
+    transferPolicy.target = event.params.sessionSpec.transferPolicies[i].target
+    transferPolicy.maxValuePerUse = event.params.sessionSpec.transferPolicies[i].maxValuePerUse
+    let valueLimit = new UsageLimit(transferPolicy.id);  
+    valueLimit.limitType = event.params.sessionSpec.transferPolicies[i].valueLimit.limitType
+    valueLimit.limit = event.params.sessionSpec.transferPolicies[i].valueLimit.limit
+    valueLimit.period = event.params.sessionSpec.transferPolicies[i].valueLimit.period
+    valueLimit.save()
+    transferPolicy.valueLimit = valueLimit.id
+    transferPolicy.save()
+    transferPolicies.push(transferPolicy.id)
+  }
+  sessionSpec.transferPolicies = transferPolicies
+  sessionSpec.save()
+
   let entity = new SessionCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.account = event.params.account
   entity.sessionHash = event.params.sessionHash
-  entity.sessionSpec_signer = event.params.sessionSpec.signer
-  entity.sessionSpec_expiresAt = event.params.sessionSpec.expiresAt
-  entity.sessionSpec_feeLimit_limitType =
-    event.params.sessionSpec.feeLimit.limitType
-  entity.sessionSpec_feeLimit_limit = event.params.sessionSpec.feeLimit.limit
-  entity.sessionSpec_feeLimit_period = event.params.sessionSpec.feeLimit.period
-  entity.sessionSpec_callPolicies = changetype<Bytes[]>(
-    event.params.sessionSpec.callPolicies
-  )
-  entity.sessionSpec_transferPolicies = changetype<Bytes[]>(
-    event.params.sessionSpec.transferPolicies
-  )
-
+  entity.sessionSpec = sessionSpec.id
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
