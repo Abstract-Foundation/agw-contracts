8093a6368ab563dc86e50dc6aca7f04b419cdb92
Coffee☕️ <coffee@coffeebot.dev>
2025-02-19 16:12:34 -0500

    add pragma

--- contracts/helpers/SessionKeyRegistry.sol
+++ contracts/helpers/SessionKeyRegistry.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
